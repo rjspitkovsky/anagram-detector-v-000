a81fbe6fc92d388f5a8d8a9910bef60d25d1e505
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-31 20:40:47 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,17 @@
 class Anagram
+  attr_accessor :word
 
+  def initialize(word)
+    @word = word
+  end
+
+  def match(potential_anagrams)
+    array = []
+    potential_anagrams.each do |word|
+      if word.sort == @word.sort
+        array << word
+      end
+  end
+array
 end 
+end
